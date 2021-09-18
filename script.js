d271e741c8d504d28b7d326fa286db2d9389baf2
Phil <happy.melodist@gmail.com>
2021-09-18 13:20:10 -0500

    add javascript code to footer element for dynamic year change

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
+// magic cards
+
 document.querySelectorAll(".story-btn").forEach((boop) => {
   boop.addEventListener("click", () => {
     boop.classList.toggle("schpadoinkle");
     boop.nextElementSibling.classList.toggle("schpadoinkle");
   });
 });
+
+// copyright
+
+const yearEl = document.querySelector(".year");
+const currentYear = new Date().getFullYear();
+yearEl.textContent = currentYear;
